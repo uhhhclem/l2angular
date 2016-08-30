7ef3dd2529138f6ea3cac4941d814b22cb86e971
Robert Rossney <rrossney@gmail.com>
2016-08-30 13:31:52 -0700

    Update app.controller.js
      The world of angular examples is full of bad habits.  Don't replicate those habits.

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,6 +1,22 @@
-angular.module('firstCtrl', []).controller('CalculatorController', function CalculatorController($scope) {
+// create the module separately from the controller, and give it a more appropriate name
+// You'll be instantiating it in HTML like this:
+//
+// <div ng-app="myApp">
+var app = angular.module('myApp', []);
+
+// create the controller function separately from adding it to the module:
+var ctrl = function($scope) {
 	$scope.message = "hello World"
-    $scope.sum = function() {
-          $scope.z = $scope.x + $scope.y;
-  };
-});
+	$scope.sum = function() {
+        $scope.z = $scope.x + $scope.y;
+};
+
+// Now add the controller to the app.
+app.controller('CalculatorController', ctrl);
+
+// Protip:  minification will break the controller.  To keep minification from breaking your code, angular lets you provide 
+// names of the injected services along with the constructor:
+//
+// app.controller('CalculatorController', ['$scope', ctrl]);
+//
+// You should always do this.
